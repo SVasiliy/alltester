ddb35b87191da2ad00c43ac2515787ff82e03899
SVasiliy <vasiliy.sharapov@gmail.com>
2018-11-02 20:19:45 -0500

    added controller

--- src/main/java/alltester/Example.java
+++ src/main/java/alltester/Example.java
@@ -4,15 +4,10 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@SpringBootApplication
 @EnableAutoConfiguration
 public class Example {
 	
-	@RequestMapping("/")
-	String home() {
-		return "start again 2";
-	}
-
 	public static void main(String[] args) {
 		
 		SpringApplication.run(Example.class, args);
