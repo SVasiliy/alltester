325567f05ab8f1afbc6b9a5519512713bfd1c4cd
SVasiliy <vasiliy.sharapov@gmail.com>
2018-12-25 12:40:48 -0600

    jenkins pipeline test

--- src/main/java/model/UserList.java
+++ src/main/java/model/UserList.java
@@ -11,6 +11,7 @@ public class UserList {
 		userList.add(new User(1,"user1","username1","user1@gmail.com", new Address()));
 		userList.add(new User(2,"user2","username2","user2@gmail.com", new Address("street 1"," suite 1","Somecity","99999")));
 		userList.add(new User(3,"user3","username3","user3@gmail.com", new Address()));
+		userList.add(new User(3,"user4","username4","user4@gmail.com", new Address()));
 		
 		return userList;
 		
