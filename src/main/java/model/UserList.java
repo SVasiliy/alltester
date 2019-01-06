06ef8d14d3d9dd7e2d768fa8043704953f8b9b01
SVasiliy <vasiliy.sharapov@gmail.com>
2019-01-06 14:07:00 +0100

    retest backend pipeline

--- src/main/java/model/UserList.java
+++ src/main/java/model/UserList.java
@@ -8,8 +8,8 @@ public class UserList {
 	public static List<User> getUserList(){
 		List<User> userList = new ArrayList<User>();
 		
-		userList.add(new User(1,"user1","username1","user1@gmail.com", new Address()));
-		userList.add(new User(2,"user2","username2","user2@gmail.com", new Address("street 1"," suite 1","Somecity","99999")));
+		userList.add(new User(1,"user1","username1","user1@gmail.com", new Address("street 1"," suite 1","Somecity","99999")));
+		userList.add(new User(2,"user2","username2","user2@gmail.com", new Address("street 2"," suite 2","Somecity2","22222")));
 		userList.add(new User(3,"user3","username3","user3@gmail.com", new Address()));
 		userList.add(new User(4,"user4","username4","user4@gmail.com", new Address()));
 		userList.add(new User(5,"user5","username5","user5@gmail.com", new Address()));
