43c8c826dbba957781b13ac44c4a2c74a23aded4
SVasiliy <vasiliy.sharapov@gmail.com>
2019-01-15 09:18:31 -0600

    userlist update

--- src/main/java/model/UserList.java
+++ src/main/java/model/UserList.java
@@ -8,11 +8,11 @@ public class UserList {
 	public static List<User> getUserList(){
 		List<User> userList = new ArrayList<User>();
 		
-		userList.add(new User(1,"user1","username1","user1@gmail.com", new Address("street 1"," suite 1","Somecity","99999")));
-		userList.add(new User(2,"user2","username2","user2@gmail.com", new Address("street 2"," suite 2","Somecity2","22222")));
-		userList.add(new User(3,"user3","username3","user3@gmail.com", new Address()));
-		userList.add(new User(4,"user4","username4","user4@gmail.com", new Address()));
-		userList.add(new User(5,"user5","username5","user5@gmail.com", new Address()));
+		userList.add(new User(1,"jcarta","Jolanda Carta","jcarta@gmail.com", new Address("780 Pleasant Circle","","Waco","75705")));
+		userList.add(new User(2,"rcuriel","Rana Curiel","rcuriel@gmail.com", new Address("90 Rocky River Lane","","El Paso","79907")));
+		userList.add(new User(3,"nkimmell","Narcisa Kimmell","nkimmell@gmail.com", new Address("13 Theatre St.","","Houston","77081")));
+		userList.add(new User(4,"lblay","Lia Blay","lblay@gmail.com", new Address("767 County St.","","Lake Jackson","77566")));
+		userList.add(new User(5,"acurtsinger","Alita Curtsinger","acurtsinger@gmail.com", new Address("74 Logan St.","","El Paso","79927")));
 		
 		return userList;
 		
