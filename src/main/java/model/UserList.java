f406fcce23c7031016bfaa6a1e00add4f4c51129
SVasiliy <vasiliy.sharapov@gmail.com>
2019-01-20 09:00:16 -0600

    operational commit

--- src/main/java/model/UserList.java
+++ src/main/java/model/UserList.java
@@ -8,7 +8,7 @@ public class UserList {
 	public static List<User> getUserList(){
 		List<User> userList = new ArrayList<User>();
 		
-		userList.add(new User(1,"jcarta","Jolanda Carta","jcarta@gmail.com", new Address("780 Pleasant Circle","","Waco","75705")));
+		userList.add(new User(1,"jcarta","Jolanda Carta","jcarta@gmail.com", new Address("781 Pleasant Circle","","Waco","75705")));
 		userList.add(new User(2,"rcuriel","Rana Curiel","rcuriel@gmail.com", new Address("90 Rocky River Lane","","El Paso","79907")));
 		userList.add(new User(3,"nkimmell","Narcisa Kimmell","nkimmell@gmail.com", new Address("13 Theatre St.","","Houston","77081")));
 		userList.add(new User(4,"lblay","Lia Blay","lblay@gmail.com", new Address("767 County St.","","Lake Jackson","77566")));
