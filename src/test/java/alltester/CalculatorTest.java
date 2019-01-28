a801859ffaa2a7bd2c009d6556cc8c32c8c82a49
SVasiliy <vasiliy.sharapov@gmail.com>
2019-01-27 19:27:59 -0600

    add JUnit 4.12

---
+++ src/test/java/alltester/CalculatorTest.java
@@ -0,0 +1,17 @@
+package alltester;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import core.Calculator;
+
+public class CalculatorTest {
+	
+  @Test
+  public void evaluatesExpression() {
+    Calculator calculator = new Calculator();
+    int sum = calculator.evaluate("1+2+3");
+    assertEquals(6, sum);
+  }
+  
+}
